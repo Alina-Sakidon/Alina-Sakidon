a56e0fbca2c2df808c1e92c04b46457c16b70181
Alina Sakidon <alinas@devart.com>
2021-07-27 11:46:37 +0300

    Removed old file

---
+++ Calculator Home task/conteiners/Animal.java
@@ -0,0 +1,10 @@
+package conteiners;
+
+public class Animal {
+    public void move(){
+        System.out.println("Animal moves");
+    }
+    public String saySmth(){
+        return "sound";
+    }
+}
