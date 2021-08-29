0ee5cddc72313c9e3762919454518820e2e3bc49
Alina Sakidon <alinas@devart.com>
2021-08-29 22:11:20 +0300

    ui tests home-task14

--- src/test/java/ua/hillel/tests/Hovers.java
+++ src/test/java/ua/hillel/tests/Hovers.java
@@ -2,7 +2,6 @@ package ua.hillel.tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -13,28 +12,17 @@ public class Hovers extends Base {
     @Test
     public void getNameByHover() {
         driver.get("https://the-internet.herokuapp.com/hovers");
-        Actions actions = new Actions(driver);
         List<WebElement> cont = driver.findElements(By.xpath(path));
-        for (int i = 0; i < cont.size(); i++) {
-            actions.moveToElement(cont.get(i)).perform();
-            printName(String.format("[%1s]//h5", i + 1));
+        int i = 1;
+        for (WebElement userIcon :
+                cont) {
+            actions.moveToElement(userIcon).perform();
+            printName(String.format("[%1s]//h5", i++));
         }
     }
     private void printName(String loc){
         System.out.println(driver.findElement(By.xpath(path+loc)).getText());
     }
-        /*WebElement firstIcon = driver.findElement(By.xpath("//div[@class='example']/div[1]"));
-        actions.moveToElement(firstIcon).perform();
-        System.out.println(driver.findElement(By.xpath("//div[@class='example']/div[1]//h5")).getText());
-
-        WebElement secondIcon = driver.findElement(By.xpath("//div[@class='example']/div[2]"));
-        actions.moveToElement(secondIcon).perform();
-        System.out.println(driver.findElement(By.xpath("//div[@class='example']/div[2]//h5")).getText());
-
-        WebElement thirdIcon = driver.findElement(By.xpath("//div[@class='example']/div[3]"));
-        actions.moveToElement(thirdIcon).perform();
-        System.out.println(driver.findElement(By.xpath("//div[@class='example']/div[3]//h5")).getText());
-    }*/
 }
 
 
--- src/test/java/ua/hillel/tests/LoginPage.java
+++ src/test/java/ua/hillel/tests/LoginPage.java
@@ -1,76 +1,47 @@
 package ua.hillel.tests;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class LoginPage {
-    private WebDriver prepareLoginForm(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
+public class LoginPage extends Base {
+    protected String cleanTextOfLogin;
+    private void prepareLoginForm(){
         driver.get("https://the-internet.herokuapp.com/login");
-        return driver;
     }
-    @Test(priority = 1)
-    public void TestLoginValid() throws InterruptedException {
-        WebDriver driver=prepareLoginForm();
-        WebElement userName=driver.findElement(By.xpath("//h4[@class='subheader']/em[1]"));
+    private  void loginUser(String userName,String password){
         WebElement inputUserName = driver.findElement(By.xpath("//input[@type='text']"));
         inputUserName.clear();
-        WebElement password =driver.findElement(By.xpath("//h4[@class='subheader']/em[2]"));
         WebElement inputPassword = driver.findElement(By.xpath("//input[@type='password']"));
         inputPassword.clear();
-        inputUserName.sendKeys(userName.getText());
-        inputPassword.sendKeys(password.getText());
+        inputUserName.sendKeys(userName);
+        inputPassword.sendKeys(password);
         driver.findElement(By.xpath("//button[@type='submit']")).click();
-        Thread.sleep(1000);
-        String textOfSuccesLogin = driver.findElement(By.cssSelector("#flash")).getText();
-        String messageSymbol = driver.findElement(By.xpath("//a[contains(@class,'close')]")).getText();
-        String cleanTextOfLogin =textOfSuccesLogin.replace("\n" + messageSymbol,"");
-        Assert.assertEquals(cleanTextOfLogin,"You logged into a secure area!");
-        driver.quit();
+    }
+    private String getLoginMessage(){
+        return driver.findElement(By.cssSelector("#flash")).getText();
+    }
+
+    @Test(priority = 1)
+    public void TestLoginValid(){
+        prepareLoginForm();
+        loginUser("tomsmith","SuperSecretPassword!");
+        Assert.assertTrue(getLoginMessage().contains("You logged into a secure area!"));
     }
 
     @Test(priority = 5)
-    public void TestInvalidPassword() throws InterruptedException {
-        WebDriver driver=prepareLoginForm();
-        WebElement userName=driver.findElement(By.xpath("//h4[@class='subheader']/em[1]"));
-        WebElement inputUserName = driver.findElement(By.xpath("//input[@type='text']"));
-        inputUserName.clear();
-        WebElement password =driver.findElement(By.xpath("//h4[@class='subheader']/em[2]"));
-        WebElement inputPassword = driver.findElement(By.xpath("//input[@type='password']"));
-        inputPassword.clear();
-        inputUserName.sendKeys(userName.getText());
-        inputPassword.sendKeys(password.getText()+"5");
-        driver.findElement(By.xpath("//button[@type='submit']")).click();
-        Thread.sleep(1000);
-        String textOfInvalidLogin = driver.findElement(By.cssSelector("#flash")).getText();
-        String messageSymbol = driver.findElement(By.xpath("//a[contains(@class,'close')]")).getText();
-        String cleanTextOfLogin = textOfInvalidLogin.replace("\n" + messageSymbol,"");
-        Assert.assertEquals(cleanTextOfLogin,"Your password is invalid!");
-        driver.quit();
+    public void TestInvalidPassword(){
+       prepareLoginForm();
+        loginUser("tomsmith","SuperSecretPassword!222");
+        Assert.assertTrue(getLoginMessage().contains("Your password is invalid!"));
     }
+
     @Test(priority = 10)
-    public void TestInvalidUserName() throws InterruptedException {
-        WebDriver driver=prepareLoginForm();
-        WebElement userName=driver.findElement(By.xpath("//h4[@class='subheader']/em[1]"));
-        WebElement inputUserName = driver.findElement(By.xpath("//input[@type='text']"));
-        inputUserName.clear();
-        WebElement password =driver.findElement(By.xpath("//h4[@class='subheader']/em[2]"));
-        WebElement inputPassword = driver.findElement(By.xpath("//input[@type='password']"));
-        inputPassword.clear();
-        inputUserName.sendKeys(userName.getText()+"5");
-        inputPassword.sendKeys(password.getText());
-        driver.findElement(By.xpath("//button[@type='submit']")).click();
-        Thread.sleep(1000);
-        String textOfInvalidLogin = driver.findElement(By.cssSelector("#flash")).getText();
-        String messageSymbol = driver.findElement(By.xpath("//a[contains(@class,'close')]")).getText();
-        String cleanTextOfLogin = textOfInvalidLogin.replace("\n" + messageSymbol,"");
-        Assert.assertEquals(cleanTextOfLogin,"Your username is invalid!");
-        driver.quit();
+    public void TestInvalidUserName(){
+        prepareLoginForm();
+        loginUser("tomsmith222","SuperSecretPassword!");
+        Assert.assertTrue(getLoginMessage().contains("Your username is invalid!"));
     }
 }
